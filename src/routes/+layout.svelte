1f56c23f5e2aaccca0b8ba703348afe14e3dbabe
Gurkan <hello@gurkz.me>
2023-07-20 21:26:08 +0200

    fix the progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,10 @@
 	import Header from '../components/Header.svelte';
 	import '../styles/global.css';
 	import NProgress from 'nprogress';
+    import 'nprogress/nprogress.css';
 
 	NProgress.configure({
-		minimum: 0.16
+		minimum: 0.16,
 	});
 
 	$: {
