6df538ae119d0e8450fc35a9d57da2c9460559d3
Gurkan <hello@gurkz.me>
2023-07-21 15:09:22 +0200

    change to custom api

--- src/lib/minecraft.ts
+++ src/lib/minecraft.ts
@@ -3,32 +3,32 @@ import { z } from 'zod';
 
 const statusSchema = z.object({
     online: z.boolean(),
-    status: z.boolean(),
-    favicon_base64: z.string(),
-    favicon: z.string().url(),
-    source: z.string(),
-    took: z.number(),
-    cache: z.object({
-        status: z.string(),
-        ttl: z.number(),
-        insertion_time: z.string().datetime(),
-    }),
     version: z.object({
         name: z.string(),
-        protocol: z.number(),
+        protocol: z.number()
     }),
     players: z.object({
         max: z.number(),
-        online: z.number(),
+        online: z.number()
     }),
     description: z.string(),
-    fetch: z.string().datetime(),
+    favicon: z.string().optional(),
+    ping: z.number().optional(),
 })
+
+export type StatusOptions = {
+    /** @default true */
+    checkPing?: boolean;
+    /** @default 5000 // ms */
+    timeout?: number;
+    /** @default 763 // 1.20.1 */
+    protocol?: number;
+}
 
 export type Status = z.infer<typeof statusSchema>
 
 export async function getStatus(address: string) {
-    const res = await axios.get<Status>(`https://eu.mc-api.net/v3/server/ping/${address}`);
+    const res = await axios.get<Status>(`https://gurkz.me/minecraft/status/${address}`);
     const { data } = await res;
     return data;
 }
---
+++ src/routes/minecraft/status/[address]/+server.ts
@@ -0,0 +1,72 @@
+import type { Status, StatusOptions } from "$lib/minecraft";
+import { Client, PacketWriter, State } from "mcproto";
+import type { RequestHandler } from "./$types";
+import { error, json } from "@sveltejs/kit";
+
+const defaultOptions: Partial<StatusOptions> = {
+    checkPing: true,
+    timeout: 5000,
+    protocol: 736,
+};
+
+export const GET = (async ({ params }) => {
+    const { address } = params;
+
+    const client = await Client.connect(address, 25565, {
+        connectTimeout: defaultOptions.timeout,
+        timeout: defaultOptions.timeout
+    }).catch((err) => console.error("connection error", err.stack));
+
+    if (!client) {
+        throw error(404, JSON.stringify({
+            online: false,
+            version: {
+                name: "",
+                protocol: 0,
+            },
+            players: {
+                max: 0,
+                online: 0,
+            },
+            description: "",
+        }))
+    } else {
+        client.send(
+            new PacketWriter(0x0)
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                .writeVarInt(defaultOptions.protocol!)
+                .writeString(address)
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                .writeUInt16(client.socket.remotePort!)
+                .writeVarInt(State.Status)
+        )
+
+        client.send(new PacketWriter(0x0));
+
+        const status: Status = (await client.nextPacket()).readJSON();
+
+        status.online = true;
+
+        if (defaultOptions.checkPing) {
+            client.send(new PacketWriter(0x1).write(Buffer.alloc(8)));
+            const start = Date.now();
+
+            await client.nextPacket(0x1);
+            status.ping = Date.now() - start;
+        }
+
+        client.end();
+
+        const options: ResponseInit = {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Cache-Control": "max-age=0, s-maxage=1, stale-while-revalidate"
+            }
+        }
+
+        return json(status, options)
+    }
+
+    return new Response(String("uwu"));
+}) satisfies RequestHandler
